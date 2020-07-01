47cca67014d11694aabf997be1cd49a1b0c57e7e
Usman Ahmad <usman.ahmad@gosign.de>
2020-07-01 22:50:18 +0200

    adds linear regression to dataframe

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
-
+from pyspark.ml.regression import LinearRegression
+from pyspark.ml.linalg import Vectors
+from pyspark.ml.feature import VectorAssembler
+from pyspark.sql.types import DoubleType
 
 def main():
 
@@ -23,23 +26,25 @@ def main():
                        '1/25/20', '1/26/20', '1/27/20', '1/28/20', '1/29/20', '1/30/20', '1/31/20', '3/22/20', '3/23/20']
     df = df.drop(*columns_to_drop)
     # sum up all rows data into 1 row
-    # df.select(F.sum("2/1/20").alias('2/1/20'),
-    # F.sum("2/2/20")).show()  # its todo
-    new_df = df.select([F.sum(value).alias(str(index))
-                        for index, value in enumerate(df.columns)]).show()
-    new_df.show()
-    # if want to save to a csv
-    # new_df.write.option('header', 'true').csv('data.csv')
-
-    # print(df.agg({"3/21/20": "sum"}).collect())
-    # df.groupBy().sum().show()
-    # df.show()
-
-    # clean data
-    ####################################
+    df = df.select([F.sum(value).alias(str(index))
+                        for index, value in enumerate(df.columns)])
+    # transpose the dataframe
+    df_p = df.toPandas().transpose().reset_index()
+    df_p.rename(columns={0:'Infections'})
+    df_s = spark.createDataFrame(df_p)
+    df_s = df_s.select(F.col('index'), F.col("0").alias("Infections"))
 
     # linear regression
     ####################################
+    df_s = df_s.withColumn("index_double", df_s['index'].cast(DoubleType()))
+    featureassembler = VectorAssembler(inputCols=["index_double"],outputCol="new_index")
+    output=featureassembler.transform(df_s)
+    finalized_data = output.select("new_index","Infections")
+    train_data,test_data = finalized_data.randomSplit([0.75,0.25])
+    regressor = LinearRegression(featuresCol='new_index', labelCol='Infections')
+    regressor = regressor.fit(train_data)
+    pred_results=regressor.evaluate(test_data)
+    pred_results.predictions.show(40)
 
     # visualize results
     ####################################
